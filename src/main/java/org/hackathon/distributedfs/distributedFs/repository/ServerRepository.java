b7d41cfb5076566e271b32920fe4115a9c61814f
Rahul Ahuja <rahulahuja404@gmail.com>
2018-10-05 15:39:49 +0530

    added ServerRepository.java

---
+++ src/main/java/org/hackathon/distributedfs/distributedFs/repository/ServerRepository.java
@@ -0,0 +1,7 @@
+package org.hackathon.distributedfs.distributedFs.repository;
+
+import org.hackathon.distributedfs.distributedFs.model.Server;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ServerRepository extends CrudRepository<Server,Long> {
+}
